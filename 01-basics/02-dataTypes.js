8adb35430ec2c4126f2d8ad0d6a811216d5bba7c
HarshDixitt <126702565+HarshDixitt@users.noreply.github.com>
2024-02-19 10:35:36 +0000

    datatypes in js

---
+++ 01-basics/02-dataTypes.js
@@ -0,0 +1,29 @@
+"use strict"; // treat all JS code as newer version
+
+// alert(10 + 10) // we are using nodejs
+
+// console.log(3  +  3) // code readability should be high
+
+// console.log("Hitesh")
+
+
+let name = "hitesh"
+let age = 18
+let isLoggedIn = false
+let state;
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+
+// object
+
+console.log(typeof name); // string 
+console.log(typeof age); // number
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
